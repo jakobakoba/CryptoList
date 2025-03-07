c452d7d080c323cfb0357890c1e42e35c11b98c0
jakobakoba <borisgabyshev96dev@gmail.com>
2025-03-07 19:00:59 +0900

    setup hilt and ksp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.hilt.android)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -64,4 +66,11 @@ dependencies {
     implementation(libs.navigation)
 
     implementation(libs.viewmodel.compose)
+
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
+    implementation(libs.hilt.navigation.compose)
+
+
+
 }
